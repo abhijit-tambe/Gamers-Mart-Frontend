aa834429e928507e5ab9df4385f5598f6ab80661
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-14 19:43:39 -0400

    App created

---
+++ gamers-mart/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import GamersMartApp from './components/App/GamersMartApp';
+
+
+function App() {
+  return (
+    <div className="App">
+       <GamersMartApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ gamers-mart/src/components/App/GamersMartApp.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+import './GamersMartApp.css'
+
+class GamersMartApp extends Component{
+
+render(){
+    return(
+        <div className="GMApp"> welcome to Gamers Mart</div>
+    );
+}
+
+}
+
+export default GamersMartApp;
