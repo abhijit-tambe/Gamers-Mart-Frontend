678345a39e6f326828c78e436880d0e5a9068591
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-15 14:45:54 -0400

    Update GamersMartApp.jsx

--- gamers-mart/src/components/App/GamersMartApp.jsx
+++ gamers-mart/src/components/App/GamersMartApp.jsx
@@ -1,15 +1,23 @@
 import React,{Component} from 'react'
 import './GamersMartApp.css'
 import Login from '../Login/Login'
-
+import Home from '../Home/Home'
+import {BrowserRouter as Router, Route} from 'react-router-dom' 
 class GamersMartApp extends Component{
 
 render(){
     return(
-        <>
-        <div className="GMApp"> welcome to Gamers Mart</div>
-        <Login/>
-        </>
+        
+        <div className="GMApp"> welcome to Gamers Mart
+        <Router>
+            <>
+            <Route path="/" exact component={Login}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/home/:name" component={Home}></Route>
+            </>        
+        </Router>
+
+        </div>
     );
 }
 
