a28c7128fc094c957c23b4386757ddcebf8dcfee
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-14 20:46:36 -0400

    Update GamersMartApp.jsx

--- gamers-mart/src/components/App/GamersMartApp.jsx
+++ gamers-mart/src/components/App/GamersMartApp.jsx
@@ -1,11 +1,15 @@
 import React,{Component} from 'react'
 import './GamersMartApp.css'
+import Login from '../Login/Login'
 
 class GamersMartApp extends Component{
 
 render(){
     return(
+        <>
         <div className="GMApp"> welcome to Gamers Mart</div>
+        <Login/>
+        </>
     );
 }
 
