3460ace80b7fe925cbbf79d1315bc2c99bc63d9e
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-15 14:45:49 -0400

    Update Login.jsx

--- gamers-mart/src/components/Login/Login.jsx
+++ gamers-mart/src/components/Login/Login.jsx
@@ -1,17 +1,29 @@
 import React,{Component} from 'react'
 import './Login.css'
+
+import AuthenticateUser from '../../Authentication/AuthenticationService.js'
+
 class Login extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state={
             username:"",
-            password:""
+            password:"",
+            loginFailed: false,
+            successMessage: false
         }
 
-        this.changeInput= this.changeInput.bind(this)
+        this.changeInput= this.changeInput.bind(this);
+        this.loginCheck =this.loginCheck.bind(this);
+        this.logout = this.logout.bind(this);
 
     }
 
+    logout(){
+        if(AuthenticateUser.isUserLoggedIn()){
+            AuthenticateUser.userlogout();
+        }
+    }
 
     changeInput(event){
         this.setState({
@@ -19,12 +31,35 @@ class Login extends Component{
         })
     }
 
+    loginCheck(){
+        AuthenticateUser.AuthenticateUser(this.state.username,this.state.password);
+        if(AuthenticateUser.isUserLoggedIn()){
+            console.log('suc')
+            // this.setState({
+            //     loginFailed:false,
+            //     successMessage: true,
+            // })
+            this.props.history.push(`/Home/${this.state.username}`);
+        }else{
+            console.log('fail')
+            this.setState({
+                successMessage:false,
+                loginFailed: true
+            })
+        }
+        
+        
+           
+    }
 
     render(){
+        
         return(
             <div className="container">
                 <h1>Login</h1>
-           <div >
+                {this.state.loginFailed && <h3> Login Failed Invalid Username or password</h3>}
+                {this.state.successMessage &&<h3>Loggedin Successfull</h3>}
+           <div>
                <label>
                    User Name  <br/>
                <input type="text" name="username" value={this.state.username} onChange={this.changeInput}></input>
@@ -34,7 +69,8 @@ class Login extends Component{
                    Password <br/>
                <input type="password" name="password" value={this.state.password} onChange={this.changeInput}></input>
                </label>
-               
+               <button className="btn btn-md btn-primary" onClick={this.loginCheck}>Login</button>
+               <button className="btn btn-md btn-primary" onClick={this.logout}>Logout</button>
            </div>
            </div>
         );
