e5507f4509520f37bab2ad837116fa5381613ab1
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-16 16:57:02 -0400

    Update AuthenticationService.js

--- gamers-mart/src/Authentication/AuthenticationService.js
+++ gamers-mart/src/Authentication/AuthenticationService.js
@@ -1,4 +1,4 @@
-import UserDataService from '../Api/UserDataService.js'
+
 
 
 export const USER_NAME_SESSION_ATTRIBUTE_NAME='AuthenticatedUser';
@@ -7,14 +7,6 @@ class AuthenticationService {
     AuthenticateUser(username,password){
         var user = 'abhijit';
         var pass = 'tambe';
-        //  UserDataService.getAllUsers()
-        // .then(res=>{
-        //     res.data.filter(x=>{
-        //       if(x.userName=== username && x.password===password){
-        //         console.log(x.password);
-        //         sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME,username);
-        //         }
-        //       })})
         if(username === user && password === pass){
           sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME,username);
         }
@@ -23,14 +15,13 @@ class AuthenticationService {
 
     isUserLoggedIn(){
         let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
-        if(user===null)return false
-     return true
+        if(user===null)return false;
+     return true;
     }
 
     userlogout(){
         sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
-        console.log('logged out')
-       
+        console.log('logged out');
     }
 
 }
