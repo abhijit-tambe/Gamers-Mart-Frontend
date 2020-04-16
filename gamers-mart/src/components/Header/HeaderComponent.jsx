56dc3f74a2f1f991781b0e9201c401d80792f137
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-16 18:32:06 -0400

    Update HeaderComponent.jsx

--- gamers-mart/src/components/Header/HeaderComponent.jsx
+++ gamers-mart/src/components/Header/HeaderComponent.jsx
@@ -2,18 +2,24 @@ import React,{ Component } from "react";
 import  AuthenticationService from '../../Authentication/AuthenticationService.js'
 import { Link } from "react-router-dom";
 import {withRouter} from 'react-router';
+import USER_NAME_SESSION_ATTRIBUTE_NAME from '../../Authentication/AuthenticationService.js'
+
 
 class HeaderComponent extends Component{
     render(){
+        var islogged = AuthenticationService.isUserLoggedIn();
+        
         return(
             <header>
                 <nav className="navbar navbar-dark bg-dark navbar-expand-md">
-                    <div> <a href="abhijittambe.com" className="navbar-brand">abhijit tambe</a></div>
+                    <div> <a href="abhijittambe.com" className="navbar-brand">GamersMart</a></div>
                     <ul className="navbar-nav">
-                        <li><Link to="/login" className="nav-link">Sign In</Link></li>
+                        <li><Link className="nav-link" to="/marketplace">Market Place</Link></li>
+                        {islogged && <li><Link className="nav-link" to="home/:name" >Dashboard</Link></li>}
                     </ul>
                     <ul className="navbar-nav  navbar-collapse justify-content-end">
-                        <li> <Link className="nav-link" to="/logout" onClick={AuthenticationService.userlogout}>Sign out</Link></li>
+                        {!islogged && <li><Link to="/login" className="nav-link">Sign In</Link></li>}
+                        {islogged && <li> <Link className="nav-link" to="/logout" onClick={AuthenticationService.userlogout}>Sign out</Link></li>}
                         </ul>
                 </nav>
             </header>
