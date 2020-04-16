4f8974c63e9aba562d2de3c934646ac5545fd902
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-15 22:21:40 -0400

    Update GamersMartApp.jsx

--- gamers-mart/src/components/App/GamersMartApp.jsx
+++ gamers-mart/src/components/App/GamersMartApp.jsx
@@ -5,6 +5,7 @@ import ErrorComponent from './Error/ErrorComponent'
 import SignInComponent from '../SignIn/SignInComponent'
 import UserHomeComponent from '../Home/UserHomeComponent'
 import HeaderComponent from '../Header/HeaderComponent'
+import SignOutComponent from '../SignOut/SignOutComponent'
 class GamersMartApp extends Component{
 
 render(){
@@ -18,6 +19,7 @@ render(){
             <Route path="/" exact component={SignInComponent}></Route>
             <Route path="/login" component={SignInComponent}></Route>
             <Route path="/home/:name" component={UserHomeComponent}></Route>
+            <Route path="/logout" component={SignOutComponent}/>
             <Route component={ErrorComponent}/>
             </Switch>        
         </Router>
