347acdb9f9d4201deab0429e8cf1f5d956e4a6f0
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-17 21:07:00 -0400

    Update GamersMartApp.jsx

--- gamers-mart/src/components/App/GamersMartApp.jsx
+++ gamers-mart/src/components/App/GamersMartApp.jsx
@@ -3,12 +3,26 @@ import './GamersMartApp.css'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ErrorComponent from '../Error/ErrorComponent' 
 import SignInComponent from '../SignIn/SignInComponent'
-import UserHomeComponent from '../Home/UserHomeComponent'
+import UserHomeComponent from '../UserHome/UserHomeComponent'
 import HeaderComponent from '../Header/HeaderComponent'
 import SignOutComponent from '../SignOut/SignOutComponent'
 import MarketPlaceComponent from '../Marketplace/MarketplaceComponent'
 import MyListingComponent from '../MyListing/MyListingComponent'
+import HomepageComponent from '../Homepage/HomepageComponent'
+import AuthenticationService from '../../Authentication/AuthenticationService.js'
+
+
 class GamersMartApp extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            isLogged: AuthenticationService.isUserLoggedIn(),
+            userName: AuthenticationService.getUserName(),
+        }
+    }
+
+    
 
 render(){
     return(
@@ -18,7 +32,7 @@ render(){
         <Router>
         <HeaderComponent />
             <Switch>
-            <Route path="/" exact component={SignInComponent}></Route>
+            <Route path="/" exact component={HomepageComponent}></Route>
             <Route path="/login" component={SignInComponent}></Route>
             <Route path="/home" component={UserHomeComponent}></Route>
             <Route path="/marketplace" component={MarketPlaceComponent}/>
