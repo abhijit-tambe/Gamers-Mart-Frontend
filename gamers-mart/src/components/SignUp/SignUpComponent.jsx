46e12fde9be824394770591db9973c2a17912ada
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-20 17:57:28 -0400

    Update SignUpComponent.jsx

--- gamers-mart/src/components/SignUp/SignUpComponent.jsx
+++ gamers-mart/src/components/SignUp/SignUpComponent.jsx
@@ -7,21 +7,47 @@ class SignUpComponent extends Component{
     render(){
         return(
             <div>
+              <div className="signuplogo"> <p>Sign Up for <span className="gamersMart">GamersMart</span> </p></div>
             <form>
   <div class="row">
     <div className="col-3"></div>
     <div class="col-6">
-      <label>Email
-      <input type="text" className="form-control" placeholder="Your email address"/></label>
+      <label>Email</label>
+      <input type="text" className="form-control" placeholder="Your email address"/>
     </div>
     <div className="col-3"></div>
     <div className="col-3"></div>
     <div class="col-6">
-    <label>Firstname</label>
-      <input type="text" className="form-control" placeholder="Your firstname"/>
+    <label>Username</label>
+      <input type="text" className="form-control" placeholder="Pick a username"/>
+    </div>
+    <div className="col-3"></div>
+    <div className="col-3"></div>
+    <div class="col-6">
+    <label>Password</label>
+      <input type="text" className="form-control" placeholder="Select a password"/>
+    </div>
+    <div className="col-3"></div>
+    <div className="col-3"></div>
+    <div class="col-6">
+    <label>Retype Password</label>
+      <input type="text" className="form-control" placeholder="Retype the above password"/>
+    </div>
+    <div className="col-3"></div>
+    <div className="col-3"></div>
+    <div class="col-6">
+    <label>Phone</label>
+      <input type="text" className="form-control" placeholder="Your cell number"/>
+    </div>
+    <div className="col-3"></div>
+    <div className="col-3"></div>
+    <div class="col-6">
+    {/* <label>Sign Up</label> */}
+      <button  className="btn btn-success btn-lg signupbtn" >Sign Up</button>
     </div>
   </div>
-</form></div>
+</form>
+</div>
         );
 
     }
