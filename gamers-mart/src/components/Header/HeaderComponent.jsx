2aae5d4ef939a48b968099a6387f2b5fceec17f6
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-21 22:06:07 -0400

    Update HeaderComponent.jsx

--- gamers-mart/src/components/Header/HeaderComponent.jsx
+++ gamers-mart/src/components/Header/HeaderComponent.jsx
@@ -8,6 +8,7 @@ import USER_NAME_SESSION_ATTRIBUTE_NAME from '../../Authentication/Authenticatio
 class HeaderComponent extends Component{
     render(){
         var islogged = AuthenticationService.isUserLoggedIn();
+        var user = AuthenticationService.getUserName();
         
         return(
             <header>
@@ -15,7 +16,7 @@ class HeaderComponent extends Component{
                     <div> <Link to="/" className="navbar-brand">GamersMart</Link></div>
                     <ul className="navbar-nav">
                         <li><Link className="nav-link" to="/marketplace">Marketplace</Link></li>
-                        {islogged && <li><Link className="nav-link" to="/home" >Dashboard</Link></li>}
+                        {islogged && <li><Link className="nav-link" to={`/${user}`} >Dashboard</Link></li>}
                         {islogged && <li><Link className="nav-link" to="/mylistings" >My Listings</Link></li>}
                     </ul>
                     <ul className="navbar-nav  navbar-collapse justify-content-end">
