62d1ba99311439e72d0e3a8fa58a0bd05f1db711
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-21 21:30:17 -0400

    Update SignInComponent.jsx

--- gamers-mart/src/components/SignIn/SignInComponent.jsx
+++ gamers-mart/src/components/SignIn/SignInComponent.jsx
@@ -40,7 +40,7 @@ class SignInComponent extends Component{
             //     loginFailed:false,
             //     successMessage: true,
             // })
-            this.props.history.push(`/home`);
+            this.props.history.push(`/home/${this.state.username}`);
         }else{
             console.log('fail')
             this.setState({
