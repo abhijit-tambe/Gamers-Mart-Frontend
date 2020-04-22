0c1e38887ae22a15808c9e2b40be68a2278be1b9
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-22 18:42:47 -0400

    Update AuthenticationService.js

--- gamers-mart/src/Authentication/AuthenticationService.js
+++ gamers-mart/src/Authentication/AuthenticationService.js
@@ -1,38 +1,36 @@
-
-
-
-export const USER_NAME_SESSION_ATTRIBUTE_NAME='AuthenticatedUser';
+import axios from "axios";
+export const USER_NAME_SESSION_ATTRIBUTE_NAME = "AuthenticatedUser";
 class AuthenticationService {
+  createUser(st) {
+    console.log(st);
+    return axios.post("http://localhost:8080//create-account", st);
+  }
 
-    AuthenticateUser(username,password){
-        var user = 'abhijit';
-        var pass = 'tambe';
-        if(username === user && password === pass){
-          sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME,username);
-        }
+  AuthenticateUser(username, password) {
+    var user = "abhijit";
+    var pass = "tambe";
+    if (username === user && password === pass) {
+      sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
     }
+  }
 
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
+    if (user === null) return false;
+    return true;
+  }
 
-    isUserLoggedIn(){
-        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
-        if(user===null)return false;
-     return true;
-    }
+  userlogout() {
+    sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
+    console.log("logged out");
+  }
 
-    userlogout(){
-        sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
-        console.log('logged out');
-    }
-
-
-    getUserName(){
-        if(this.isUserLoggedIn()){
-            let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
-            return user;
-        }else
-        return null; 
-    }
-
+  getUserName() {
+    if (this.isUserLoggedIn()) {
+      let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
+      return user;
+    } else return null;
+  }
 }
 
 export default new AuthenticationService();
