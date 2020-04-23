40a8a442ab95d29ebc6b76807c1c6e59e902ea07
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-22 21:40:43 -0400

    Update SignUpComponent.jsx

--- gamers-mart/src/components/SignUp/SignUpComponent.jsx
+++ gamers-mart/src/components/SignUp/SignUpComponent.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./SignUpComponent.css";
+import { Link } from "react-router-dom";
 import AuthenticationService from "../../Authentication/AuthenticationService";
 
 class SignUpComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // status: null,
+      complete: false,
+      failed: false,
       st: {
         emailId: "",
         userName: "",
@@ -34,9 +36,19 @@ class SignUpComponent extends Component {
 
   dD() {
     console.log(this.state);
-    AuthenticationService.createUser(this.state.st).then((res) => {
-      console.log(res.status);
-    });
+    AuthenticationService.createUser(this.state.st)
+      .then((res) => {
+        console.log("status:" + res);
+
+        this.setState({
+          complete: true,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          failed: true,
+        });
+      });
   }
 
   changeData(event) {
@@ -52,56 +64,68 @@ class SignUpComponent extends Component {
     return (
       <div className="signupmain">
         {/* <div className="col-3"></div> */}
+
         <div className="formarea col-12">
           {/* <form> */}
-          <div className="row d-flex justify-content-center">
-            {/* <div className="col-3"></div> */}
-            <div className="signuplogo col-7">
+          {this.state.failed && <div> failed signup try again </div>}
+          {this.state.complete && (
+            <div>
               {" "}
-              <p>
-                Sign Up for <span className="gamersMart">GamersMart</span>{" "}
-              </p>
+              Registered successfully redirect to{" "}
+              <Link className="link" to="/login">
+                signin
+              </Link>{" "}
             </div>
-            <div className="col-7">
-              <label>Email</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Your email address"
-                name="emailId"
-                value={this.state.st.emailId}
-                onChange={this.changeData}
-              />
-            </div>
-            {/* <div className="col-3"></div>
+          )}
+          {!this.state.complete && (
+            <div className="row d-flex justify-content-center">
+              {/* <div className="col-3"></div> */}
+              <div className="signuplogo col-7">
+                {" "}
+                <p>
+                  Sign Up for <span className="gamersMart">GamersMart</span>{" "}
+                </p>
+              </div>
+              <div className="col-7">
+                <label>Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Your email address"
+                  name="emailId"
+                  value={this.state.st.emailId}
+                  onChange={this.changeData}
+                />
+              </div>
+              {/* <div className="col-3"></div>
+              <div className="col-3"></div> */}
+              <div className="col-7">
+                <label>Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Pick a username"
+                  name="userName"
+                  value={this.state.st.userName}
+                  onChange={this.changeData}
+                />
+              </div>
+              {/* <div className="col-3"></div>
+                <div className="col-3"></div> */}
+              <div className="col-7">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Select a password"
+                  name="password"
+                  value={this.state.st.password}
+                  onChange={this.changeData}
+                />
+              </div>
+              {/* <div className="col-3"></div>
     <div className="col-3"></div> */}
-            <div className="col-7">
-              <label>Username</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Pick a username"
-                name="userName"
-                value={this.state.st.userName}
-                onChange={this.changeData}
-              />
-            </div>
-            {/* <div className="col-3"></div>
-    <div className="col-3"></div> */}
-            <div className="col-7">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Select a password"
-                name="password"
-                value={this.state.st.password}
-                onChange={this.changeData}
-              />
-            </div>
-            {/* <div className="col-3"></div>
-    <div className="col-3"></div> */}
-            {/* <div className="col-12">
+              {/* <div className="col-12">
               <label>Retype Password</label>
               <input
                 type="text"
@@ -109,33 +133,35 @@ class SignUpComponent extends Component {
                 placeholder="Retype the above password"
               />
             </div> */}
-            {/* <div className="col-3"></div>
+              {/* <div className="col-3"></div>
     <div className="col-3"></div> */}
-            <div className="col-7">
-              <label>Phone</label>
-              <input
-                type="number"
-                className="form-control"
-                placeholder="Your cell number"
-                name="phoneNumber"
-                value={this.state.st.phonenumber}
-                onChange={this.changeData}
-              />
-            </div>
-            {/* <div className="col-3"></div>
+              <div className="col-7">
+                <label>Phone</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Your cell number"
+                  name="phoneNumber"
+                  value={this.state.st.phonenumber}
+                  onChange={this.changeData}
+                />
+              </div>
+              {/* <div className="col-3"></div>
     <div className="col-3"></div> */}
-            <div className="col-7">
-              {/* <label>Sign Up</label> */}
-              <button
-                onClick={this.dD}
-                className="btn btn-success  signupbtn btn-block"
-              >
-                Create account
-              </button>
+              <div className="col-7">
+                {/* <label>Sign Up</label> */}
+                <button
+                  onClick={this.dD}
+                  className="btn btn-success  signupbtn btn-block"
+                >
+                  Create account
+                </button>
+              </div>
             </div>
-          </div>
+          )}
           {/* </form> */}
         </div>
+
         {/* <div className="col-3"></div> */}
       </div>
     );
