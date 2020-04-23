5a762b5b894daab828888f7d643908a9f3596cb2
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-22 21:18:04 -0400

    Update GamersMartApp.jsx

--- gamers-mart/src/components/App/GamersMartApp.jsx
+++ gamers-mart/src/components/App/GamersMartApp.jsx
@@ -1,55 +1,48 @@
-import React,{Component} from 'react'
-import './GamersMartApp.css'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import ErrorComponent from './ErrorComponent' 
-import SignInComponent from '../SignIn/SignInComponent'
-import UserHomeComponent from '../UserHome/UserHomeComponent'
-import HeaderComponent from '../Header/HeaderComponent'
-import SignOutComponent from '../SignOut/SignOutComponent'
-import MarketPlaceComponent from '../Marketplace/MarketplaceComponent'
-import MyListingComponent from '../MyListing/MyListingComponent'
-import HomepageComponent from '../Homepage/HomepageComponent'
-import AuthenticationService from '../../Authentication/AuthenticationService.js'
-import SignUpComponent from '../Homepage/SignUpComponent.jsx'
+import React, { Component } from "react";
+import "./GamersMartApp.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ErrorComponent from "./ErrorComponent";
+import SignInComponent from "../SignIn/SignInComponent";
+import UserHomeComponent from "../UserHome/UserHomeComponent";
+import HeaderComponent from "../Header/HeaderComponent";
+import SignOutComponent from "../SignOut/SignOutComponent";
+import MarketPlaceComponent from "../Marketplace/MarketplaceComponent";
+import MyListingComponent from "../MyListing/MyListingComponent";
+import HomepageComponent from "../Homepage/HomepageComponent";
+import AuthenticationService from "../../Authentication/AuthenticationService.js";
+import SignUpComponent from "../SignUp/SignUpComponent.jsx";
 
+class GamersMartApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogged: AuthenticationService.isUserLoggedIn(),
+      userName: AuthenticationService.getUserName(),
+    };
+  }
 
-class GamersMartApp extends Component{
-
-    constructor(props){
-        super(props);
-        this.state={
-            isLogged: AuthenticationService.isUserLoggedIn(),
-            userName: AuthenticationService.getUserName(),
-        }
-    }
-
-    
-
-render(){
-    return(
-        
-        <div className="GMApp"> 
-       
+  render() {
+    return (
+      <div className="GMApp">
         <Router>
-        <HeaderComponent />
+          <HeaderComponent />
+          <div className="container">
             <Switch>
-            <Route path="/" exact component={HomepageComponent}></Route>
-            
-            <Route path="/login" component={SignInComponent}></Route>
-            <Route path="/marketplace" component={MarketPlaceComponent}/>
-            <Route path="/logout" component={SignOutComponent}/>
-            <Route path="/mylistings" component={MyListingComponent}/>
-            <Route path="/signup/:email" component={SignUpComponent}/>
-            <Route path="/signup" component={SignUpComponent}/>
-            <Route path="/:name" component={UserHomeComponent}></Route>
-            <Route component={ErrorComponent}/>
-            </Switch>        
+              <Route path="/" exact component={HomepageComponent}></Route>
+              <Route path="/login" component={SignInComponent}></Route>
+              <Route path="/marketplace" component={MarketPlaceComponent} />
+              <Route path="/logout" component={SignOutComponent} />
+              <Route path="/mylistings" component={MyListingComponent} />
+              <Route path="/signup/:email" component={SignUpComponent} />
+              <Route path="/signup" component={SignUpComponent} />
+              <Route path="/:name" component={UserHomeComponent}></Route>
+              <Route component={ErrorComponent} />
+            </Switch>
+          </div>
         </Router>
-
-        </div>
+      </div>
     );
-}
-
+  }
 }
 
 export default GamersMartApp;
