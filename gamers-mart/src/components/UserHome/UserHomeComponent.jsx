f24e417f9bbc0233f299774963af9bf612a75366
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-29 21:08:22 -0400

    Update UserHomeComponent.jsx

--- gamers-mart/src/components/UserHome/UserHomeComponent.jsx
+++ gamers-mart/src/components/UserHome/UserHomeComponent.jsx
@@ -1,33 +1,35 @@
-import React,{ Component } from "react";
-import AuthenticationService, { USER_NAME_SESSION_ATTRIBUTE_NAME } from '../../Authentication/AuthenticationService.js'
+import React, { Component } from "react";
+import AuthenticationService, {
+  USER_NAME_SESSION_ATTRIBUTE_NAME,
+} from "../../Authentication/AuthenticationService.js";
 
+class UserHomeComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-class UserHomeComponent extends Component{
-    constructor(props){
-        super(props)
-        
-    }
+  componentDidMount() {
+    this.setState((state, props) => ({
+      isLogged: AuthenticationService.getUserName(),
+    }));
+  }
 
-
-    componentDidMount(){
-        this.setState((state,props) =>({
-               isLogged: AuthenticationService.getUserName(),
-        }))
-    }
-
-
-    render(){
-        var user = AuthenticationService.getUserName();
-        var islogged = AuthenticationService.isUserLoggedIn();
-        return(
-            <>
-            { islogged && <div>
-                welcome home {this.props.match.params.name}
-            </div>}
-            { !islogged && <div>public profile of {this.props.match.params.name}</div>}
-            </>
-        )
-    }
+  render() {
+    var user = AuthenticationService.getUserName();
+    var islogged = AuthenticationService.isUserLoggedIn();
+    var paramname = this.props.match.params.name;
+    var validity = user === paramname ? true : false;
+    return (
+      <>
+        {islogged && validity && (
+          <div>welcome home {this.props.match.params.name}</div>
+        )}
+        {!islogged || validity || (
+          <div>public profile of {this.props.match.params.name}</div>
+        )}
+      </>
+    );
+  }
 }
 
 export default UserHomeComponent;
