78229cef9c9e6e8f2fe69b7e1613e4af631495ca
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-05-01 18:48:03 -0400

    Update MyItemCard.jsx

--- gamers-mart/src/components/MyListing/MyItemCard.jsx
+++ gamers-mart/src/components/MyListing/MyItemCard.jsx
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
-
+import "./MyItemCard.css";
 class MyItemCard extends Component {
   render() {
-    // var x  = [];
-    // x = [...this.props.datax];
-
-    // console.log(x);
-    // console.log(this.props.datax);
     return (
-      <div className="row container">
-        {this.props.datax.map((x) => (
-          <div className="col-md-4" key={x.itemId}>
-            <div className="card">
-              <div className="card-body">
-                <h3 className="card-title">{x.itemName}</h3>
-                <h5 className="card-feature">Game :{x.gameName}</h5>
-                <div className="card-text">
-                  <p>Type:{x.itemType} </p>
-                  <p> Description:{x.description}</p>
-                  <p className="bold">Cost:{x.price}</p>
+      <>
+        <div className="additem row">
+          <div className="outerbox">
+            <div className="innerbox">
+              <button className="btn btn-lg btn-primary">Add Item</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="row container">
+          {this.props.datax.map((x) => (
+            <div className="col-md-4" key={x.itemId}>
+              <div className="card">
+                <div className="card-body">
+                  <h3 className="card-title">{x.itemName}</h3>
+                  <h5 className="card-feature">Game :{x.gameName}</h5>
+                  <div className="card-text">
+                    <p>Type:{x.itemType} </p>
+                    <p> Description:{x.description}</p>
+                    <p className="bold">Cost:{x.price}</p>
+                  </div>
+                  <button>Edit Listing</button>
+                  <button>Remove Listing</button>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </>
     );
   }
 }
